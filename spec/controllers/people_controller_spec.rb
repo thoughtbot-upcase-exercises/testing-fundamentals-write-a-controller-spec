1fdf400a98b9c8202c729e65a89a2c0d4c727a11
Joshua Clayton <jclayton@thoughtbot.com>
2015-05-26 22:38:27 -0400

    Test the #create action

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -4,13 +4,35 @@ describe PeopleController do
   describe "#create" do
     context "when person is valid" do
       it "redirects to #show" do
-        pending "create this test"
+        person = stub_valid_person
+
+        post :create, person: { first_name: "John" }
+
+        expect(response).to redirect_to(person)
       end
     end
 
     context "when person is invalid" do
-      it "redirects to #new" do
-        pending "create this test"
+      it "renders new" do
+        person = stub_invalid_person
+
+        post :create, person: { first_name: "John" }
+
+        expect(response).to render_template(:new)
+      end
+    end
+
+    def stub_valid_person
+      Person.new.tap do |person|
+        allow(Person).to receive(:new).and_return person
+        allow(person).to receive(:save).and_return(true)
+      end
+    end
+
+    def stub_invalid_person
+      Person.new.tap do |person|
+        allow(Person).to receive(:new).and_return person
+        allow(person).to receive(:save).and_return(false)
       end
     end
   end
